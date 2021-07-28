9aa10d5ff6431fe6f8d36c06a46660afe8070aaf
arthur <arhurdeveloper@gmail.com>
2021-07-27 23:15:30 -0300

    finalizado home da pagina

---
+++ script/index.js
@@ -0,0 +1,10 @@
+(function () {
+    const img = document.querySelector('p > a > img')
+    if(innerWidth <= 1366)
+        img.style.marginLeft = '70%'
+    else 
+        img.style.marginLeft = '65%'
+    console.log('teste')
+
+})()
+
